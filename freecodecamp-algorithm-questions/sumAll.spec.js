7d5cfbee24379b4c7c7a77e11f8d04811f75a21d
Anirudh <anirudh.mangalvedhekar1989@gmail.com>
2018-06-28 01:57:40 -0400

    fix

--- freecodecamp-algorithm-questions/sumAll.spec.js
+++ freecodecamp-algorithm-questions/sumAll.spec.js
@@ -5,6 +5,7 @@ import {
     getMinimumValue,
     getMaximumValue,
     generateNumbers,
+    sumAll
 } from './sumAll';
 
 const minimumNumberFromArray = () => {
@@ -13,8 +14,38 @@ const minimumNumberFromArray = () => {
     assert.equal(actualValue, expectedValue);
 };
 
+const maximumNumberFromArray = () => {
+    const actualValue = getMaximumValue([1,2]);
+    const expectedValue = 2;
+    assert.equal(actualValue, expectedValue);
+};
+
+const numberRangeGenerated = () => {
+    const actualValue = generateNumbers(1,9);
+    const expectedValue = [1,2,3,4,5,6,7,8,9];
+    assert.deepEqual(actualValue, expectedValue);
+};
+
+const sumForSequenceNumbers = () => {
+    const actualValue = sumAll([1,4]);
+    const expectedValue = 10;
+    assert.equal(actualValue, expectedValue);
+};
+
 describe('sumAll', () => {
+
     it('should return minimum value for an array',
-        minimumNumberFromArray
+        minimumNumberFromArray,
     );
+
+    it('should return maximum value for an array',
+        maximumNumberFromArray,
+    );
+
+    it('should return an array with sequence of numbers generated',
+        numberRangeGenerated,
+    );
+
+    it('should return sum for sequence of numbers',
+        sumForSequenceNumbers);
 });
