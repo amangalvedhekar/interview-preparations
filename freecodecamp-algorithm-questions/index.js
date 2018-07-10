8fd12b67ecc94bfa4564e54b00c35415b36c075d
Anirudh <amangalvedhekar@icloud.com>
2018-07-09 21:40:24 -0400

    demo added in index

--- freecodecamp-algorithm-questions/index.js
+++ freecodecamp-algorithm-questions/index.js
@@ -1,9 +1,11 @@
 import { diffArray } from './diffArray';
 import { seekAndDestroy} from './seekAndDestroy';
 import { sumAll } from './sumAll';
+import  whatIsInAName   from './whatIsInAName';
 
 export {
     diffArray,
     seekAndDestroy,
     sumAll,
+    whatIsInAName,
 };
--- index.js
+++ index.js
@@ -2,8 +2,18 @@ import {
     diffArray,
     seekAndDestroy,
     sumAll,
+    whatIsInAName,
 } from './freecodecamp-algorithm-questions';
 
 console.log(sumAll([1, 4]));
 console.log(diffArray([1, 2, 3, 4, 5], [1, 2, 3, 5, 6]));
 console.log(seekAndDestroy([1, 2, 3, 1, 2, 3], 2, 3));
+console.log(whatIsInAName(
+    [
+        { "apple": 1, "bat": 2 },
+        { "apple": 1 },
+        { "apple": 1, "bat": 2, "cookie": 2 },
+        { "bat":2 }
+    ],
+    {"apple": 1, "bat": 2 }
+));
