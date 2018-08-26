3c307d9d12ce7d602cc6c1c847461195f1b7a420
Anirudh <amangalvedhekar@icloud.com>
2018-08-27 00:51:18 +0530

    rosseta code challenges setup

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 * [Algorithm Scripting](https://github.com/amangalvedhekar/interview-preparations/tree/master/freecodecamp-algorithm-questions)
 * [JavaScript Algorithms and Data Structures Projects]()
 * [Miscellaneous Code Snippets](https://github.com/amangalvedhekar/interview-preparations/tree/master/miscellaneous-code-snippets)
+* [Rosetta Code]()
---
+++ miscellaneous-code-snippets/findPangram.js
@@ -0,0 +1,8 @@
+/**
+ * Pangram is a sentence that has each letter of alphabet
+ * There are two ways to approach this check:
+ *    1. Create a hash table of each character occurence.
+ *    2. filter each character of the sentence across string with all
+ *        alphabets and if the length of this filter is 0 then the given sentence is pangram
+ *
+ */
